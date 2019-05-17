62227ea987dfd8e99f1eae7d3c8ab4e19ce12879
yzabc007 <neilwang1992@gmail.com>
2019-05-16 21:16:14 -0400

    update

--- src/train_surfcon.sh
+++ src/train_surfcon.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 CUDA_VISIBLE_DEVICES=2 python3 -u main_dym.py \
---restore_model_path='./saved_models/saved_pretrained/ns_snapshot_epoch_9.pt' \
---neg_sampling=True \
+--restore_model_path='./saved_models/saved_pretrained/snapshot_epoch_2000.pt' \
+--neg_sampling=False \
 --num_contexts=100 \
 
 
