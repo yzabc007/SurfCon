3058c06516d8e01a940607fa3d973b4a80178c63
yzabc007 <neilwang1992@gmail.com>
2019-05-27 15:38:47 -0400

    change gpu flag

--- src/train_surfcon.sh
+++ src/train_surfcon.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=2 python3 -u main_dym.py \
---restore_model_path='./saved_models/saved_pretrained/snapshot_epoch_2000.pt' \
---neg_sampling=False \
+CUDA_VISIBLE_DEVICES=3 python -u main_dym.py \
+--restore_model_path='./saved_models/saved_pretrained_fast/ns_snapshot_epoch_134.pt' \
+--neg_sampling=True \
 --num_contexts=100 \
 
 
