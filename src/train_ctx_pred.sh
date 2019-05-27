8d08c5926a72d19d1041ee18fea59b3d0c824b54
yzabc007 <neilwang1992@gmail.com>
2019-05-27 15:40:48 -0400

    change gpu flag

--- src/train_ctx_pred.sh
+++ src/train_ctx_pred.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=1 python -u main_pretrain.py \
+CUDA_VISIBLE_DEVICES=0 python -u main_pretrain.py \
 --neg_sampling=True \
 --batch_size=10000 \
 --learning_rate=0.001 \
--- src/train_surfcon.sh
+++ src/train_surfcon.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=3 python -u main_dym.py \
+CUDA_VISIBLE_DEVICES=0 python -u main_dym.py \
 --restore_model_path='./saved_models/saved_pretrained_fast/ns_snapshot_epoch_134.pt' \
 --neg_sampling=True \
 --num_contexts=100 \
