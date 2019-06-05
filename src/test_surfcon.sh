90601385e2d6b29af6d7ab0bbf7309e57c0228eb
yzabc007 <neilwang1992@gmail.com>
2019-06-05 16:31:18 -0400

    add candidate terms

--- src/test_surfcon.sh
+++ src/test_surfcon.sh
@@ -3,4 +3,5 @@
 python -u main_testing.py \
 --restore_model_path='./saved_models/saved_pretrained/snapshot_epoch_2000.pt' \
 --rank_model_path='./saved_models/rank_model_perBin_1/best_epoch_18.pt' \
---num_results=10
+--num_results=10 \
+--cand_terms_path=''
